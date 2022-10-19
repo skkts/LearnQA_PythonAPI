1d478b7a3c7bd00b0e9570002a0a8bca5cb9852c
Tanya <skkts31@gmail.com>
2022-10-19 15:19:42 +0500

    user authorize tests

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,7 +1,9 @@
 import pytest
 import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
 
-class TestUserAuth:
+class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
         ("no_token")
@@ -12,16 +14,11 @@ class TestUserAuth:
             "email": "vinkotov@example.com",
             "password": "1234"
         }
-
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
-        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
-        assert "user_id" in response1.json(), "There is no user id in the response"
-
-        self.auth_sid = response1.cookies.get('auth_sid')
-        self.token = response1.headers.get("x-csrf-token")
-        self.user_id_from_auth_method = response1.json()["user_id"]
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_headers(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
         response2 = requests.get(
@@ -30,11 +27,12 @@ class TestUserAuth:
             cookies={"auth_sid": self.auth_sid}
         )
 
-        assert "user_id" in response2.json(), "There is no user id in the response"
-
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert self.user_id_from_auth_method == user_id_from_check_method, "User IDs doesn't match"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User IDs doesn't match"
+        )
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
@@ -49,8 +47,9 @@ class TestUserAuth:
                 cookies={"auth_sid": self.auth_sid}
             )
 
-        assert "user_id" in response2.json(), "There is no user id in the response"
-
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with condition {condition}"
+        )
